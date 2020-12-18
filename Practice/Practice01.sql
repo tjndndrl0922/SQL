397cd256aacee7b58b0568cb4a25ce85df265ca9
'SeoWoongki' <'tjndndrl0922@naver.com'>
2020-12-18 18:28:05 +0900

    Practice01

--- Practice/Practice01.sql
+++ Practice/Practice01.sql
@@ -6,7 +6,7 @@
 이름(first_name last_name),  월급(salary),  전화번호(phone_number), 입사일(hire_date) 순서이고 
 “이름”, “월급”, “전화번호”, “입사일” 로 컬럼이름을 대체해 보세요.
 */
-SELECT first_name 이름,
+SELECT first_name || ' ' ||last_name 이름,
         salary 월급,
         phone_number 전화번호,
         hire_date 입사일
@@ -17,10 +17,10 @@ order by hire_date asc;
 문제2.
 업무(jobs)별로 업무이름(job_title)과 최고월급(max_salary)을 월급의 내림차순(DESC)로 정렬하세요.
 */
-SELECT job_id,
-        salary
-FROM employees
-order by salary desc;
+SELECT job_title,
+        max_salary
+FROM jobs
+order by max_salary desc;
 
 /*
 문제3.
@@ -38,14 +38,14 @@ and salary > 3000;
 
 /*
 문제4.
-최고월급(max_salary)이 10000 이상인 업무의 이름(job_title)과 최고월급(max_salary)을 최고월급의(max_salary) 내림차순(DESC)로 정렬하여 출력하세요.    
+최고월급(max_salary)이 10000 이상인 업무의 이름(job_title)과 
+최고월급(max_salary)을 최고월급의(max_salary) 내림차순(DESC)로 정렬하여 출력하세요.    
 */
-SELECT job_id,
-        salary
-FROM employees
-where salary > 10000
-order by salary desc;
-
+SELECT job_title,
+        max_salary
+FROM jobs
+where max_salary >= 10000
+order by max_salary desc;
 /*
 문제5.
 월급이 14000 미만 10000 이상인 직원의 이름(first_name), 월급, 커미션퍼센트 를  월급순(내림차순) 출력하세오.
@@ -56,7 +56,7 @@ SELECT first_name,
         nvl(commission_pct,0)
 FROM employees
 where salary < 14000
-and salary >10000
+and salary >= 10000
 order by salary desc;
 
 /*
